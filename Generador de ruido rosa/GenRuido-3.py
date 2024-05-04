33ddfbe2359e1c15f044510e690d9a6609760c3c
valen_rode <valenrode2000@gmail.com>
2024-05-04 11:18:39 -0300

    Gen ruido rosa rode

---
+++ Generador de ruido rosa/GenRuido-3.py
@@ -0,0 +1,61 @@
+def ruidoRosa_voss(nrows, ncols=16):
+    """
+    Genera ruido rosa utilizando el algoritmo de Voss-McCartney(https://www.dsprelated.com/showabstract/3933.php).
+    
+    .. Nota:: si 'ruidoRosa.wav' existe, este será sobreescrito
+    
+    Parametros
+    ----------
+    t : float
+        Valor temporal en segundos, este determina la duración del ruido generado.
+    rcols: int
+        Determina el número de fuentes a aleatorias a agregar.
+    fs: int
+        Frecuencia de muestreo en Hz de la señal. Por defecto el valor es 44100 Hz.
+    
+    returns: NumPy array
+        Datos de la señal generada.
+    
+    Ejemplo
+    -------
+    Generar un `.wav` desde un numpy array de 10 segundos con ruido rosa a una 
+    frecuencia de muestreo de 44100 Hz.
+    
+        import numpy as np
+        import soundfile as sf
+        from scipy import signal
+        
+        ruidoRosa_voss(10)
+    """
+    import pandas as pd
+    import numpy as np
+    array = np.full((nrows, ncols), np.nan)
+    array[0, :] = np.random.random(ncols)
+    array[:, 0] = np.random.random(nrows)
+    
+    # el numero total de cambios es nrows
+    n = nrows
+    cols = np.random.geometric(0.5, n)
+    cols[cols >= ncols] = 0
+    rows = np.random.randint(nrows, size=n)
+    array[rows, cols] = np.random.random(n)
+    
+    df = pd.DataFrame(array)
+    filled = df.ffill(axis=0)
+    total = filled.sum(axis=1)
+    
+    ## Centrado de el array en 0
+    total = total - total.mean()
+    
+    ## Normalizado
+    valor_max = max(abs(max(total)),abs(min(total)))
+    total = total / valor_max
+    
+    # Agregar generación de archivo de audio .wav
+    
+    return total
+import numpy as np
+import soundfile as sf
+from scipy import signal
+        
+ruidoRosa_voss(10)
